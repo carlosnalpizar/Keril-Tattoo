d60ec0d576d6e81cb913eb5b7772503919641c8b
Carlos Navarrete Alpizar <116604168+carlosnalpizar@users.noreply.github.com>
2024-02-26 17:07:16 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="Portafolio" element={<Portafolio />} />
-      <Route path="*" element={<NotFound />} />
+      <Route path="NotFound" element={<NotFound />} />
     </Routes>
   );
 }
